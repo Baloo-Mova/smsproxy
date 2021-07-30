c95cf0622d2fd8ab69848d3a30b5889b323cc445
dnc <dnc.sergey.mova@gmail.com>
2021-07-30 12:41:01 +0300

    Completed task

--- main.go
+++ main.go
@@ -1,42 +1,21 @@
 package main
 
 import (
-	"fmt"
-	"sync"
+	"flag"
+	"log"
+	"net/http"
+
+	"gitlab.com/devskiller-tasks/messaging-app-golang/restapi"
 )
 
 func main() {
-	// var port = flag.Int("port", 8080, "port")
-	// flag.Parse()
+	var port = flag.Int("port", 8080, "port")
+	flag.Parse()
 
-	// server := restapi.NewServer(*port)
-	// server.BindEndpoints()
-	// if err := server.Run(); err != http.ErrServerClosed {
-	// 	panic(err)
-	// }
-	// log.Println("shutdown: completed")
-
-	m := make([]string, 0)
-
-	m = append(m, "init")
-
-	var group sync.WaitGroup
-	group.Add(1)
-
-	fmt.Println(m)
-	go func(test []string) {
-
-		test = append(test, "go")
-		test = append(test, "routine")
-
-		defer group.Done()
-
-		fmt.Println(test)
-	}(m)
-
-	m = make([]string, 0)
-	m = append(m, "func")
-	group.Wait()
-
-	fmt.Println(m)
+	server := restapi.NewServer(*port)
+	server.BindEndpoints()
+	if err := server.Run(); err != http.ErrServerClosed {
+		panic(err)
+	}
+	log.Println("shutdown: completed")
 }
